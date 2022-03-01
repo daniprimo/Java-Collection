62970d6aa7cdc2e05c5b22f7cc0bf65cfff0e640
Daniel Lopes <daniellopes0208@hotmail.com>
2022-03-01 12:23:25 -0300

    exercicios

---
+++ src/br/dio/com/collections/map/Exercicio02.java
@@ -0,0 +1,11 @@
+package br.dio.com.collections.map;
+
+/*
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi conseguido.
+ */
+public class Exercicio02 {
+    public static void main(String[] args) {
+
+    }
+}
